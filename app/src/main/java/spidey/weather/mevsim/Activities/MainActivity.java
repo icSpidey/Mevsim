4aafa642277c2d7f06e316c7fd124fa3d7d5588a
icSpidey <icyspidey@gmail.com>
2022-07-31 00:10:25 +0300

    Mevsim Weather App

---
+++ app/src/main/java/spidey/weather/mevsim/Activities/MainActivity.java
@@ -0,0 +1,177 @@
+package spidey.weather.mevsim.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import spidey.weather.mevsim.Adapters.WeatherAdapter;
+import spidey.weather.mevsim.Models.WeatherModel;
+import spidey.weather.mevsim.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    ArrayList<WeatherModel> weatherModelArrayList;
+    WeatherAdapter weatherAdapter;
+
+    LocationManager locationManager;
+    int PERMISSION_CODE = 1;
+    String cityName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        setContentView(binding.getRoot());
+
+        weatherModelArrayList = new ArrayList<>();
+        weatherAdapter = new WeatherAdapter(this, weatherModelArrayList);
+        binding.RVWeathers.setAdapter(weatherAdapter);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+
+        }
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        cityName = getCityName(location.getLongitude(), location.getLatitude());
+        getWeatherInfo(cityName);
+
+        binding.searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = binding.editCity.getText().toString();
+                if (city.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please Enter A CityName", Toast.LENGTH_SHORT).show();
+                } else {
+                    binding.cityName.setText(cityName);
+                    getWeatherInfo(city);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_CODE){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permissions Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Please Grant Permissions", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private  String getCityName(double longitude, double latitude){
+        String cityName = "Not Found" ;
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = gcd.getFromLocation(latitude,longitude, 10);
+
+            for (Address adr : addresses) {
+                if (adr != null){
+                    String city = adr.getLocality();
+                    if (city != null && !city.equals("")){
+                        cityName = city;
+
+                    } else {
+                        Log.d("TAG","CITY NOT FOUND");
+                        Toast.makeText(this, "User City Not Found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        } catch (IOException e ){
+            e.printStackTrace();
+        }
+        return cityName;
+    }
+
+    private void getWeatherInfo (String cityName) {
+        String url = "http://api.weatherapi.com/v1/current.json?key=59fe756e53324ef7a99144310223007&q=" + cityName + "&aqi=no";
+        binding.cityName.setText(cityName);
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                binding.loading.setVisibility(View.GONE);
+                binding.RLHome.setVisibility(View.VISIBLE);
+                weatherModelArrayList.clear();
+
+                try {
+                    String temperature = response.getJSONObject("current").getString("temp_c");
+                    binding.temperature.setText(temperature + "°c");
+                    int  isDay = response.getJSONObject("current").getInt("is_day");
+                    String condition = response.getJSONObject("current").getJSONObject("condition").getString("text");
+                    String conditionIcon = response.getJSONObject("current").getJSONObject("condition").getString("icon");
+                    Picasso.get().load("http:".concat(conditionIcon)).into(binding.iconWeather);
+                    binding.weatherCondition.setText(condition);
+                    if (isDay == 1){
+                        //morning
+                        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6WJYkhfC8Ef02XujVgVSjSH6g7sZhx7hKbQ&usqp=CAU").into(binding.backGround);
+                    }else {
+                        //night
+                        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3v_J2zb0iSh5yuOUYNByYc7XHCWExr5RX6g&usqp=CAU").into(binding.backGround);
+                    }
+
+                    JSONObject forecastObj = response.getJSONObject("forecast");
+                    JSONObject forecast0 = forecastObj.getJSONArray("forecastday").getJSONObject(0);
+                    JSONArray hourArray = forecast0.getJSONArray("hour");
+
+                    for (int i = 0; i<hourArray.length(); i++){
+                        JSONObject hourObj = hourArray.getJSONObject(i);
+                        String time = hourObj.getString("time");
+                        String temper = hourObj.getString("temp_c");
+                        String img = hourObj.getJSONObject("condition").getString("icon");
+                        String wind = hourObj.getString("wind_kph");
+                        weatherModelArrayList.add(new WeatherModel(time,temper,img,wind));
+                    }
+                    weatherAdapter.notifyDataSetChanged();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Please Enter A Valid City Name", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+}
---
+++ app/src/main/java/spidey/weather/mevsim/Adapters/WeatherAdapter.java
@@ -0,0 +1,72 @@
+package spidey.weather.mevsim.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import spidey.weather.mevsim.Models.WeatherModel;
+import spidey.weather.mevsim.R;
+
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<WeatherModel> weatherModelArrayList;
+
+    public WeatherAdapter(Context context, ArrayList<WeatherModel> weatherModelArrayList) {
+        this.context = context;
+        this.weatherModelArrayList = weatherModelArrayList;
+    }
+
+    @NonNull
+    @Override
+    public WeatherAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherAdapter.ViewHolder holder, int position) {
+        WeatherModel model = weatherModelArrayList.get(position);
+        holder.temperature.setText(model.getTemperature() + "°c");
+        holder.windSpeed.setText(model.getWindSpeed() + "Km/h");
+        Picasso.get().load("http:".concat(model.getIcon())).into(holder.weatherCondition);
+        SimpleDateFormat  input = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        SimpleDateFormat output = new SimpleDateFormat("hh:mm aa");
+        try {
+            Date date = input.parse(model.getTime());
+            holder.timeView.setText(output.format(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherModelArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView windSpeed, temperature, timeView;
+        private ImageView weatherCondition;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            windSpeed = itemView.findViewById(R.id.windSpeed);
+            temperature = itemView.findViewById(R.id.temperature);
+            timeView = itemView.findViewById(R.id.timeView);
+            weatherCondition = itemView.findViewById(R.id.weatherCondition);
+        }
+    }
+}
